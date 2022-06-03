67ddf8c629e53d46bc9cd34e4df05a264320f06a
Dongwook Hwang <dwhwang423@gmail.com>
2022-06-02 22:35:30 -0400

    updated fetchData func to display the first image once

--- script.js
+++ script.js
@@ -4,29 +4,38 @@ const leftBtn = document.querySelector('.btn-left');
 const rightBtn = document.querySelector('.btn-right');
 let index = 0;
 
-var fetchData = () => {
-  fetch('https://picsum.photos/200').then((res) => {
-    images.push(res.url);
-    fig1.src = images[0];
+// fetchData(): to fetch an image from API, save in the images array, and apply the first image to img tag.
+const fetchData = () => {
+  return new Promise((r1, r2) => {
+    fetch('https://picsum.photos/200').then((res) => {
+      images.push(res.url);
+      if (images.length === 1) {
+        fig1.src = images[0];
+        console.log('done once');
+      }
+    });
   });
 };
 
-var loadImage = () => {
-  for (var i = 1; i < 10; i++) {
+// loadImage(): to execute fetchData function 10 times.
+const loadImage = () => {
+  for (var i = 0; i < 10; i++) {
     fetchData();
   }
 };
 
-loadImage();
-
+// button to slide to the next image, and once the very last image, it works nothing
 rightBtn.addEventListener('click', function () {
   if (index < images.length - 1) {
     fig1.src = images[++index];
   }
 });
 
+// button to slide to the previous image, and once the very first image, it works nothing
 leftBtn.addEventListener('click', function () {
   if (index > 0) {
     fig1.src = images[--index];
   }
 });
+
+loadImage();
